0f4a0299a0486b6e1f59d6e41cbf6c6bfbbb1b7c
LarisaShaloyan <151386958+LarisaShaloyan@users.noreply.github.com>
2024-05-06 14:27:24 +0400

    Add files via upload

---
+++ book.js
@@ -0,0 +1,17 @@
+let body=document.querySelector("body")
+let toggle=document.querySelector(".toggle")
+
+let getMode=localStorage.getItem("mode")
+if(getMode && getMode==="dark"){
+  body.classList.add("dark")
+  toggle.classList.add("active")
+}
+toggle.addEventListener("click",function(){
+  body.classList.toggle("dark")
+
+  if(!body.classList.contains("dark")){
+    return localStorage.setItem("mode","light")
+  }
+  localStorage.setItem("mode","dark")
+})
+toggle.addEventListener("click", ()=>toggle.classList.toggle("active"))
